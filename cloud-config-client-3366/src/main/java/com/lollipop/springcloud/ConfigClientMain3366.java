a6a0e4514649a1cb24f2a99fda57ce5a961cdebd
王山鹏 <wangshanpeng@telefen.com>
2021-03-16 14:10:55 +0800

    springcloud消息总线Bus

---
+++ cloud-config-client-3366/src/main/java/com/lollipop/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,20 @@
+package com.lollipop.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Description: TODO
+ * @Auther: shanpeng.wang
+ * @Create: 2021/3/16 13:40
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigClientMain3366
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ConfigClientMain3366.class,args);
+    }
+}
---
+++ cloud-config-client-3366/src/main/java/com/lollipop/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,29 @@
+package com.lollipop.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description: TODO
+ * @Auther: shanpeng.wang
+ * @Create: 2021/3/16 13:41
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController
+{
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String configInfo()
+    {
+        return "serverPort: "+serverPort+"\t\n\n configInfo: "+configInfo;
+    }
+
+}
